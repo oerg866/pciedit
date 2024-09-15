0edf3c4b025f34aab601ad6217414a2b78b01bbe
oerg866 <oerg866@googlemail.com>
2024-09-15 03:48:33 +0200

    Formatting, Axe pci_enum_dev, pci_get_next_device, pci_debug_info, add '-d'/'-l'

--- MAIN.C
+++ MAIN.C
@@ -1,11 +1,11 @@
 /*
  * PCIEDIT
  *
- * (C) 2022 Eric "Oerg866" Voirin
+ * (C) 2024 Eric Voirin (Oerg866)
  *
  * LICENSE: CC-BY-NC 3.0
  *
- * Tool to set PCI configuration registers in a given PCI device.
+ * Tool to get/set PCI configuration registers in PCI devices.
  *
  * Refer to README.MD
  */
@@ -14,140 +14,206 @@
 #include <stdlib.h>
 #include <dos.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "TYPES.H"
 #include "PCI.H"
 
-#define VERSION "0.2"
+#define VERSION "0.4"
 
-void print_regs(PCIDEVICE device) 
+static void print_regs(PCIDEVICE device)
 /* Prints the PCI config registers of a given device */
 {
-	u32 offset;
+    u32 offset;
 
-	printf("   | x0 x1 x2 x3 x4 x5 x6 x7    x8 x9 xA xB xC xD xE xF\n");
-	printf("---|---------------------------------------------------\n");
+    printf("   | x0 x1 x2 x3 x4 x5 x6 x7    x8 x9 xA xB xC xD xE xF\n");
+    printf("---|---------------------------------------------------\n");
 
-	for (offset = 0; offset <= 255; ++offset) {
-		if ((offset % 16) == 0) /* after a new line, write header column */
-			printf("%1xx | ", (int)offset >> 4);
+    for (offset = 0; offset <= 255; ++offset) {
+        if ((offset % 16) == 0) /* after a new line, write header column */
+            printf("%1xx | ", (int)offset >> 4);
 
-		/* Read and print actual PCI config register content */
-		printf("%02x ", (int)pci_read_8(device, offset));
+        /* Read and print actual PCI config register content */
+        printf("%02x ", (int)pci_read_8(device, offset));
 
-		if ((offset % 16) == 7) /* gap after 8 registers */
-			printf("   ");
+        if ((offset % 16) == 7) /* gap after 8 registers */
+            printf("   ");
 
-		if ((offset % 16) == 15) /* new line after 16 registers */
-			printf("\n");
-	}
-	printf("\n");
+        if ((offset % 16) == 15) /* new line after 16 registers */
+            printf("\n");
+    }
+    printf("\n");
 }
 
-int process_regs_txt(char *infile, PCIDEVICE device) 
+static int process_regs_txt(char *infile, PCIDEVICE device)
 /* Process register text file and set the contents accordingly */
 {
-	FILE *f = fopen(infile, "r");
-	char *line = NULL;
-	unsigned int bufsize = 0;
-	int index = 0;
-	int value = 0;
+    FILE *f = fopen(infile, "r");
+    char *line = NULL;
+    unsigned int bufsize = 0;
+    int index = 0;
+    int value = 0;
 
 
-	if (ferror(f)) {
-		printf("Could not open file %s\n", infile);
-		return 0;
-	}
+    if (ferror(f)) {
+        printf("Could not open file %s\n", infile);
+        return 0;
+    }
 
-	/* Process until the end of the file */
+    /* Process until the end of the file */
 
-	while (getline(&line, &bufsize, f) >= 0) {
-		/* expectedf ormat xx yy where xx is the register and yy is the value
-		   in hexadeicmal format, eg. ab cd */
+    while (getline(&line, &bufsize, f) >= 0) {
+        /* expectedf ormat xx yy where xx is the register and yy is the value
+           in hexadeicmal format, eg. ab cd */
 
-		sscanf(line, "%x %x", &index, &value);
-		if (index >= 0 && index <= 255 && value >= 0 && value <= 255) {
-			/* Valid line, we can write to PCI register now */
-			pci_write_8(device, index, value);
-		} else {
-			/* invalid line, ignore */
-			printf("Malformed line '%s'\n", line);
-		}
-	} 
+        sscanf(line, "%x %x", &index, &value);
+        if (index >= 0 && index <= 255 && value >= 0 && value <= 255) {
+            /* Valid line, we can write to PCI register now */
+            pci_write_8(device, index, value);
+        } else {
+            /* invalid line, ignore */
+            printf("Malformed line '%s'\n", line);
+        }
+    } 
 
-	fclose(f);
+    fclose(f);
 
-	return 1;
+    return 1;
+}
+
+static int dump(const char *dir) {
+    PCIDEVICE *device = NULL;
+    char outpath[256] = { 0, };
+    u8 config[256];
+    FILE *out_bin = NULL;
+    int device_count = 0;
+
+    printf("Dumping devices to '%s'...\n", dir);
+
+    while (NULL != (device = pci_get_next_device(device))) {
+        /* Get PCI config space to buffer */
+
+        pci_read_bytes(*device, config, 0, 256);
+
+        snprintf(outpath, sizeof(outpath), "%s\\%02x_%02x_%02x.BIN",
+            dir, device->bus, device->slot, device->func);
+
+        printf("Writing File: %s\n", outpath);
+
+        out_bin = fopen(outpath, "wb");
+
+        if (out_bin == NULL) {
+            printf("ERROR opening file, cancelling\n");
+            return -1;
+        }
+
+        fwrite(config, 256, 1, out_bin);
+
+        fclose(out_bin);
+
+        device_count++;
+    }
+
+    return device_count;
+}
+
+static void list (void) {
+    PCIDEVICE *current = NULL;
+
+    printf("[Bus:Slot:Func] [Ven:Dev] --- PCI Device List\n");
+    printf("----------------------------------------\n");
+
+    while (NULL != (current = pci_get_next_device(current))) {
+        pci_debug_info(*current);
+    }
 }
 
 int main(int argc, char *argv[]) {
-	int ven = 0;
-	int dev = 0;
-	PCIDEVICE device;
+    u16 ven = 0;
+    u16 dev = 0;
+    PCIDEVICE device;
 
-	printf("PCIEDIT Version %s\n", VERSION);
-	printf("(C)2022 Eric \"oerg866\" Voirin\n");
-	printf("Message me on Discord: EricV#9999\n");
-	printf("----------------------------------------\n");
+    printf("PCIEDIT Version %s\n", VERSION);
+    printf("(C)2024 E. Voirin (oerg866)\n");
+    printf("http://github.com/oerg866\n");
+    printf("----------------------------------------\n");
 
+    /* Note: No guarding against weird and invalid inputs are made...
+    if you wanted to you could exploit the hell out of this program's
+    poor parameter parsing. Whatever */
 
-	/* Usage when parameters are insufficient */
+    /* Check if PCI bus is accessible on this machine */
 
-	if (argc < 3) {
-		printf("PCIEDIT lets you set configuration registers\n");
-		printf("Usage: PCIEDIT.EXE <ven> <dev> [register list]\n");
-		printf("Example: PCIEDIT.EXE 10DE 2044 REGISTERS.TXT\n");
-		printf("\n");
-		printf("If register list is missing, the current configuration register contents\n");
-		printf("of this device are printed)\n");
-		return -1;
-	}
+    if (!pci_test()) {
+        printf("ERROR enumerating PCI bus. Quitting...\n");
+        return -1;
+    }
 
-	/* Note: No guarding against weird and invalid inputs are made...
-	if you wanted to you could exploit the hell out of this program's
-	poor parameter parsing. Whatever */
+    /* Check if we wanna list */
 
-	printf("\n\n");
+    if (argc == 2 && strcmp(argv[1], "-l") == 0) {
+        list();
+        return 0;
+    }
 
-	/* Check if PCI bus is accessible on this machine */
+    /* Usage when parameters are insufficient */
 
-	if (!pci_test()) {
-		printf("ERROR enumerating PCI bus. Quitting...\n");
-		return -1;
-	}
+    if (argc < 3) {
+        printf("PCIEDIT lets you get/set PCI device configuration registers\n");
+        printf("Usage:\n");
+        printf("         PCIEDIT.EXE <ven> <dev> [register list]\n");
+        printf("         PCIEDIT.EXE -d <dumpdir>\n");
+        printf("         PCIEDIT.EXE -l\n");
+        printf("Example: PCIEDIT.EXE 10DE 2044 REGISTERS.TXT\n");
+        printf("\n");
+        printf("If register list is missing, the current configuration register contents\n");
+        printf("of this device are printed)\n");
+        printf("\n");
+        printf("-d dumps the configuration registers of ALL PCI devices\n");
+        printf("   into the <dumpdir> directory\n");
+        printf("-l lists all PCI devices in the system.\n");
+        return -1;
+    }
 
-	/* Check if requested vendor / device ID is present in te system */
+    printf("\n\n");
 
-	ven = (u16)strtoul(argv[1], NULL, 16);
-	dev = (u16)strtoul(argv[2], NULL, 16); 
+    /* Check if we wanna dump */
 
-	if (!pci_find_dev_by_id(ven, dev, &device)) {
-		printf("Device %04x:%04x not found.\n", ven, dev);
-		return -1; 
-	}
+    if (strcmp(argv[1], "-d") == 0) {
+        return dump(argv[2]);
+    }
 
-	printf("\n");
+    /* Check if requested vendor / device ID is present in te system */
 
-	/* Print regs *before* changing anything */
+    ven = (u16)strtoul(argv[1], NULL, 16);
+    dev = (u16)strtoul(argv[2], NULL, 16); 
 
-	print_regs(device);
+    if (!pci_find_dev_by_id(ven, dev, &device)) {
+        printf("Device %04x:%04x not found.\n", ven, dev);
+        return -1; 
+    }
 
-	/* If parameter for register file is missing, quit here */
+    printf("\n");
 
-	if (argc == 3) {
-		return 0;
-	}
+    /* Print regs *before* changing anything */
 
-	/* Parse register file */
+    print_regs(device);
 
-	if (process_regs_txt(argv[3], device)) {
-		/* Success, print register contents *after* our modification */
-		printf("Registers AFTER writing:\n\n");
-		print_regs(device);
-		return 0;
-	} else {
-		return -1;
-	}
+    /* If parameter for register file is missing, quit here */
+
+    if (argc == 3) {
+        return 0;
+    }
+
+    /* Parse register file */
+
+    if (process_regs_txt(argv[3], device)) {
+        /* Success, print register contents *after* our modification */
+        printf("Registers AFTER writing:\n\n");
+        print_regs(device);
+        return 0;
+    } else {
+        return -1;
+    }
 
 }
-
--- PCI.C
+++ PCI.C
@@ -5,12 +5,14 @@
 #include <stdio.h>
 #include <dos.h>
 #include <conio.h>
+#include <malloc.h>
+#include <assert.h>
 
 #include "TYPES.H"
 
 
 #if !defined(outportb) && defined(_outp)
-	#define outportb _outp
+    #define outportb _outp
 #endif
 
 #if !defined(outportl)
@@ -19,204 +21,220 @@ static u32 inportl(u16 port)
 /* Emulates 32-bit I/O port reads using
    manual prefixed 32-bit instructions */
 {
-	u16 retl, reth;
-	__asm {
-		db 0x50                     /* push eax */
-		push bx
-		push dx
-		mov dx, port
-		db 0x66, 0xED               /* in  eax, dx */
-		mov retl, ax
-		db 0x66, 0xC1, 0xE8, 0x10   /* shr eax, 16 */
-		mov reth, ax
-		pop dx
-		pop bx
-		db 0x58                     /* pop eax */
-	}
-	return (u32) retl | ((u32) reth << 16);
+    u16 retl, reth;
+    __asm {
+        db 0x50                     /* push eax */
+        push bx
+        push dx
+        mov dx, port
+        db 0x66, 0xED               /* in  eax, dx */
+        mov retl, ax
+        db 0x66, 0xC1, 0xE8, 0x10   /* shr eax, 16 */
+        mov reth, ax
+        pop dx
+        pop bx
+        db 0x58                     /* pop eax */
+    }
+    return (u32) retl | ((u32) reth << 16);
 }
 
 static void outportl(u16 port, u32 value)
 /* Emulates 32-Bit I/O port writes using
    manual prefixed 32-bit instructions. */
 {
-	u16 vall = (u16) value;
-	u16 valh = (u16) (value >> 16);
+    u16 vall = (u16) value;
+    u16 valh = (u16) (value >> 16);
 
-	__asm {
-		db 0x50                     /* push eax */
-		db 0x53                     /* push ebx */
-		push dx
+    __asm {
+        db 0x50                     /* push eax */
+        db 0x53                     /* push ebx */
+        push dx
 
-		mov ax, valh
-		db 0x66, 0xC1, 0xE0, 0x10   /* shl eax, 16 */
-		mov dx, vall
+        mov ax, valh
+        db 0x66, 0xC1, 0xE0, 0x10   /* shl eax, 16 */
+        mov dx, vall
 
-		db 0x66, 0x0F, 0xB7, 0xDA   /* movzx ebx, dx */
-		db 0x66, 0x09, 0xD8         /* or eax, ebx */
+        db 0x66, 0x0F, 0xB7, 0xDA   /* movzx ebx, dx */
+        db 0x66, 0x09, 0xD8         /* or eax, ebx */
 
-		mov dx, port
-		db 0x66, 0xEF               /* out dx, eax */
+        mov dx, port
+        db 0x66, 0xEF               /* out dx, eax */
 
-		pop dx
-		db 0x5B                     /* pop ebx */
-		db 0x58                     /* pop eax */
-	}
+        pop dx
+        db 0x5B                     /* pop ebx */
+        db 0x58                     /* pop eax */
+    }
 }
 #endif
 
+static inline int pci_is_device(PCIDEVICE device) {
+    return (pci_get_vendor(device) == 0xFFFF) ? 0 : 1;
+}
+
+static inline int pci_is_multifunction_device(PCIDEVICE device) {
+    return pci_is_device(device) ? pci_read_8(device, 0x0E) >> 7 : 0;
+}
+
+void pci_debug_info(PCIDEVICE device) {
+    u8 header_type = pci_read_8(device, 0x0E);
+    printf("[%02x:%02x:%02x] [%04x:%04x] Header Type [%02x] %s\n",
+        device.bus, device.slot, device.func,
+        pci_get_vendor(device),
+        pci_get_device(device),
+        header_type,
+        header_type & 0x80 ? "(Multi-Function)" : "");
+}
 
 u32 pci_read_32(PCIDEVICE device, u32 offset)
 /* Reads DWORD from PCI config space. Assumes offset is DWORD-aligned. */
 {
-	u32 address = ((u32)device.bus << 16) | ((u32)device.slot << 11)
-		| ((u32)device.func << 8) | (offset & 0xFC)
-		| 0x80000000UL;
-	outportl(0xCF8, address);
-	return inportl(0xCFC);
+    u32 address = ((u32)device.bus << 16) | ((u32)device.slot << 11)
+        | ((u32)device.func << 8) | (offset & 0xFC)
+        | 0x80000000UL;
+    outportl(0xCF8, address);
+    return inportl(0xCFC);
 }
 
 
-u16 pci_read_16(PCIDEVICE device, u32 offset) {
+u16 pci_read_16(PCIDEVICE device, u32 offset)
 /* Reads WORD from PCI config space. Assumes offset is WORD-aligned. */
-
-	return (offset & 2) ? (u16) (pci_read_32(device, offset) >> 16)
-			: (u16) (pci_read_32(device, offset));
+{
+    return (offset & 2) ? (u16) (pci_read_32(device, offset) >> 16)
+            : (u16) (pci_read_32(device, offset));
 }
 
 u8 pci_read_8(PCIDEVICE device, u32 offset)
 /* Reads BYTE from PCI config space. */
 {
-	switch (offset & 3) {
-	case 3: return (u8) (pci_read_32(device, offset) >> 24);
-	case 2: return (u8) (pci_read_32(device, offset) >> 16);
-	case 1: return (u8) (pci_read_32(device, offset) >>  8);
-	case 0: return (u8) (pci_read_32(device, offset) >>  0);
-	default: return 0; /* to silence the compiler warning... */
-	}
+    switch (offset & 3) {
+    case 3: return (u8) (pci_read_32(device, offset) >> 24);
+    case 2: return (u8) (pci_read_32(device, offset) >> 16);
+    case 1: return (u8) (pci_read_32(device, offset) >>  8);
+    case 0: return (u8) (pci_read_32(device, offset) >>  0);
+    default: return 0; /* to silence the compiler warning... */
+    }
+}
+
+void pci_read_bytes(PCIDEVICE device, u8 *buffer, u32 offset, u32 count) {
+    u32 i;
+    for (i = 0; i < count; i++) {
+        buffer[i] = pci_read_8(device, offset + i);
+    }
 }
 
 void pci_write_32(PCIDEVICE device, u32 offset, u32 value)
 /* Writes DWORD to PCI config space. Assumes offset is DWORD-aligned. */
 {
-	u32 address = ((u32)device.bus << 16) | ((u32)device.slot << 11)
-		| ((u32)device.func << 8) | (offset & 0xFC)
-		| 0x80000000UL;
-	outportl(0xCF8, address);
-	outportl(0xCFC, value);
+    u32 address = ((u32)device.bus << 16) | ((u32)device.slot << 11)
+        | ((u32)device.func << 8) | (offset & 0xFC)
+        | 0x80000000UL;
+    outportl(0xCF8, address);
+    outportl(0xCFC, value);
 }
 
 void pci_write_16(PCIDEVICE device, u32 offset, u16 value)
 /* Writes WORD to PCI config space. Assumes offset is WORD-aligned. */
 {
-	u32 temp = pci_read_32(device, offset);
-	temp = (offset & 2) ? ((u32) value << 16) | (temp & 0xFFFF)
-			: ((u32) value) | (temp << 16);
-	pci_write_32(device, offset, temp);
+    u32 temp = pci_read_32(device, offset);
+    temp = (offset & 2) ? ((u32) value << 16) | (temp & 0xFFFF)
+            : ((u32) value) | (temp << 16);
+    pci_write_32(device, offset, temp);
 }
 
 void pci_write_8(PCIDEVICE device, u32 offset, u8 value)
 /* Writes BYTE to PCI config space. */
 {
-	u32 temp = pci_read_32(device, offset);
-	switch (offset & 3) {
-	case 3: temp = (temp & 0x00FFFFFFUL) | ((u32) value << 24); break;
-	case 2: temp = (temp & 0xFF00FFFFUL) | ((u32) value << 16); break;
-	case 1: temp = (temp & 0xFFFF00FFUL) | ((u32) value <<  8); break;
-	case 0: temp = (temp & 0xFFFFFF00UL) | ((u32) value <<  0); break;
-	}
-	pci_write_32(device, offset, temp);
+    u32 temp = pci_read_32(device, offset);
+    switch (offset & 3) {
+    case 3: temp = (temp & 0x00FFFFFFUL) | ((u32) value << 24); break;
+    case 2: temp = (temp & 0xFF00FFFFUL) | ((u32) value << 16); break;
+    case 1: temp = (temp & 0xFFFF00FFUL) | ((u32) value <<  8); break;
+    case 0: temp = (temp & 0xFFFFFF00UL) | ((u32) value <<  0); break;
+    }
+    pci_write_32(device, offset, temp);
 }
-
 
 u16 pci_get_vendor(PCIDEVICE device)
 /* Gets a PCI device's vendor ID for given device struct (bus/slot/function). */
 {
-	return pci_read_16(device, 0);
+    return pci_read_16(device, 0);
 }
 
 u16 pci_get_device(PCIDEVICE device)
 /* Gets a PCI device's device ID for given device struct (bus/slot/function). */
 {
-	if (pci_get_vendor(device) != 0xFFFF) {
-		return pci_read_16(device, 2);
-	} else {
-		return 0xFFFF;
-	}
-}
-
-int pci_enum_dev(PCIDEVICE device)
-/* Enumerates a PCI device based on device struct (bus/slot/function).
-   Prints PCI Device Vendor and ID code on screen.
-   Returns 1 if a device was found, 0 if not. */
-{
-	u16 ven = pci_get_vendor(device);
-	u16 dev;
-
-	if (ven != 0xFFFF) {
-		dev = pci_get_device(device);
-		printf("PCI Device %02x:%02x:%02x - VEN_%04x&DEV_%04x\n",
-			   (int) device.bus,
-			   (int) device.slot,
-			   (int) device.func,
-			   ven,
-			   dev);
-		return 1;
-	}
-
-	return 0;
+    if (pci_get_vendor(device) != 0xFFFF) {
+        return pci_read_16(device, 2);
+    } else {
+        return 0xFFFF;
+    }
 }
 
 int pci_find_dev_by_id(u16 ven, u16 dev, PCIDEVICE *device)
 /* Checks if a device with the given Vendor / device ID is present on the bus.
-   Returns 1 if the device is found, 0 if not. 
+   Returns 1 if the device is found, 0 if not.
    If it is, the device's bus, slot and function IDs are written to the struct
    pointed to by 'device' */
 {
-	PCIDEVICE current;
-	u16 found_ven;
-	u16 found_dev;
+    PCIDEVICE *current = NULL;
 
-	for (current.bus = 0; current.bus < PCI_BUS_MAX; ++current.bus) {
-		for (current.slot = 0; current.slot <= PCI_SLOT_MAX; ++current.slot) {
-			for (current.func = 0; current.func <= PCI_FUNC_MAX; ++current.func) {
+    while (NULL != (current = pci_get_next_device(current))) {
+        pci_debug_info(*current);
 
-				found_ven = pci_get_vendor(current);
-				found_dev = pci_get_device(current);
+        if (pci_get_vendor(*current) == ven && pci_get_device(*current) == dev) {
+            *device = *current;
+            return 1;
+        }
+    }
 
-				if (found_ven != 0xFFFF)
-					printf("%04x:%04x\n", (int)found_ven, (int)found_dev);
+    return 0;
+}
 
-				if (found_ven == ven && found_dev == dev) {
-					*device = current;
-					pci_enum_dev(current);
-					return 1;
-				}
-			}
-		}
-	}
+PCIDEVICE *pci_get_next_device(PCIDEVICE *device) {
+    /* first iteration = call with NULL pointer, we will allocate,
+       else base our search on the given device */
+    if (device == NULL) {
+        device = calloc(1, sizeof(PCIDEVICE));
+        assert(device != NULL);
+    } else {
+        goto restart; /* jump to the end of the for loop with existigng value */
+    }
 
-	return 0;
+    for (device->bus = 0; device->bus < PCI_BUS_MAX; ++device->bus) {
+        for (device->slot = 0; device->slot <= PCI_SLOT_MAX; ++device->slot) {
+            for (device->func = 0; device->func <= PCI_FUNC_MAX; ++device->func) {
+                if (pci_is_device(*device)) return device;
+
+/* Lord, for-give me for I have sinned */
+restart:
+                if (!pci_is_multifunction_device(*device)) break;
+            }
+        }
+    }
+
+    /* Last device was handled, no device found, dealloc and return NULL */
+    free(device);
+    return NULL;
 }
 
 int pci_test()
 /* Tests if the PCI config space can be accessed like we expect.
    Returns 1 if successful, 0 if not. */
 {
-	u32 test = 0;
+    u32 test = 0;
 
-	/* Concept stolen from linux kernel :P */
+    /* Concept stolen from linux kernel :P */
 
-	outportb(0xCFB, 0x01);
-	test = inportl(0xCF8);
-	outportl(0xCF8, 0x80000000UL);
+    outportb(0xCFB, 0x01);
+    test = inportl(0xCF8);
+    outportl(0xCF8, 0x80000000UL);
 
-	test = inportl(0xCF8);
+    test = inportl(0xCF8);
 
-	if (test != 0x80000000UL) {
-	   printf("ERROR while testing PCI configuration space access!\n");
-	   printf("Expected 0x80000000, got 0x%08lx\n", test);
-	   return 0;
-	}
-	return 1;
+    if (test != 0x80000000UL) {
+        printf("ERROR while testing PCI configuration space access!\n");
+        printf("Expected 0x80000000, got 0x%08lx\n", test);
+        return 0;
+    }
+    return 1;
 }
